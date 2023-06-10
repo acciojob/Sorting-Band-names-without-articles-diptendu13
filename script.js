11fe8aa7b355ab9eda236e9484d59b8539e2f9ac
DIPTENDU MANDAL <118509862+diptendu13@users.noreply.github.com>
2023-06-10 11:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let ans = bandNames.map(band);
+for (x of ans){
+	document.getElementById("band").innerHtml = <li>x</li>;
+}
 
+function band(element) {
+	if(element.includes("A", 0)){
+		return element.replace("A", "").trim();
+	}
+	else if(element.includes("An", 0)){
+		return element.replace("An", "").trim();
+	}
+	else if(element.includes("The", 0)){
+		return element.replace("The", "").trim();
+	}
+	return element;
+}
